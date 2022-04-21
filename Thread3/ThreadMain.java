2a007fd86293a5aabf2c9c30be9b53ada6cc89ca
bjigeon <bjigeon@gmail.com>
2022-04-21 20:30:17 +0900

    Initial commit

---
+++ Thread3/ThreadMain.java
@@ -0,0 +1,17 @@
+package network.Thread3;
+
+public class ThreadMain {
+	public static void main(String[] args) {
+		new Thread(() -> {
+			for (int i =0; i<500; i++) {
+				System.out.printf("t1 Thread : %d \n",i);
+			}
+		}).start();
+		
+		new Thread(() -> {
+			for (int i =0; i<500; i++) {
+				System.out.printf("t2 Thread : %d \n",i);
+			}
+		}).start();
+	}
+}
---
+++ thread/Thread2.java
@@ -0,0 +1,12 @@
+package network.thread;
+
+public class Thread2 extends Thread{
+	//작업 구현
+	public void run() {
+		for(int i =0; i<500;i++) {
+			System.out.printf("t1 Thread :; %d \n",i);
+		}
+	}
+	
+	
+}
